3e98e0381eda6d55d4ad6034b42eeb118c1110eb
AdityaGattu <gattuaditya@gmail.com>
2024-01-07 00:10:32 +0530

    Revert "Revert "Completed given 3 challenges""
    This reverts commit b7ac899048a2fb6bb54272676bb45ee7115ddfc0.

--- apps/reviews-client/src/app/app.spec.tsx
+++ apps/reviews-client/src/app/app.spec.tsx
@@ -1,5 +1,6 @@
 import { render } from '@testing-library/react';
 import App from './app';
+global.fetch = require('node-fetch');
 
 describe('App', () => {
 	it('should render successfully', () => {
