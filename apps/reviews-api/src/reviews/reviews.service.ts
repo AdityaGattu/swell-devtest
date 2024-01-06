2a7f49a9f4dcc30302b423226e2684be24429e35
New Author Name <email@domain.com>
2024-01-06 23:58:38 +0530

    Completed given 3 challenges

--- apps/reviews-api/src/reviews/reviews.service.ts
+++ apps/reviews-api/src/reviews/reviews.service.ts
@@ -5,6 +5,13 @@ import { DatabaseService } from '../database/database.service';
 export class ReviewsService {
 	constructor(private prisma: DatabaseService) {}
 
+	async getReviews() {
+		return this.prisma.review.findMany({
+			orderBy: { createdOn: 'desc' },
+			include: { user: true, company: true },
+		});
+	}
+
 	getReviewsCount() {
 		return this.prisma.review.count();
 	}
